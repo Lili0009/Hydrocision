e828bd48d9e0abcb52bc33ef24577e719b0366c9
Lian Gaile <149053015+Lili0009@users.noreply.github.com>
2024-10-31 23:33:16 +0800

    without celery results

--- notifications_app/admin.py
+++ notifications_app/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django_celery_beat.models import SolarSchedule, ClockedSchedule, IntervalSchedule, CrontabSchedule, PeriodicTask
-
+from django_celery_results.models import GroupResult, TaskResult 
 
 # Register your models here.
 from .models import BroadcastNotification
@@ -10,6 +10,8 @@ admin.site.unregister(ClockedSchedule)
 admin.site.unregister(IntervalSchedule)
 admin.site.unregister(CrontabSchedule)
 admin.site.unregister(PeriodicTask)
+admin.site.unregister(GroupResult)
+admin.site.unregister(TaskResult)
 
 class BroadcastNotificationAdmin(admin.ModelAdmin):
     list_display = ('message', 'broadcast_on')
