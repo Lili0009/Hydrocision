72e7e24800fbe15e1c64109ea0c65b5b36a76390
Lian Gaile <149053015+Lili0009@users.noreply.github.com>
2024-11-07 08:15:02 +0800

    notification

--- notifications_app/tasks.py
+++ notifications_app/tasks.py
@@ -16,13 +16,14 @@ def broadcast_notification(self, data):
             channel_layer = get_channel_layer()
             loop = asyncio.new_event_loop()
             asyncio.set_event_loop(loop)
-            loop.run_until_complete(channel_layer.group_send(
+            async_to_sync(channel_layer.group_send)(
                 "notification_broadcast",
                 {
                     'type': 'send_notification',
                     'message': json.dumps(notification.message),
-                }))
-            notification.sent = False
+                }
+            )
+            notification.sent = True
             notification.save()
             return 'Done'
 
